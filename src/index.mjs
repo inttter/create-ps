7db377dd5ba6d136d47274bf7cdebc440f50c3cc
Inter <me@inttter.com>
2024-02-14 22:36:22 +0000

    ✨ feat: add prompt to start a local git repo
    Signed-off-by: Inter <me@inttter.com>

--- src/index.mjs
+++ src/index.mjs
@@ -15,68 +15,74 @@ program
     .option('--esm', '--ecmascript', 'Creates an EcmaScript file in src/.')
     .action(async (packageName, options) => {
         try {
-            // Run npm init -y using execa
+            // runs npm init -y
             await execa('npm', ['init', '-y']);
 
-            // Prompt user for description
+            // description prompt
             const { description } = await inquirer.prompt({
                 type: 'input',
                 name: 'description',
                 message: chalk.cyan(`Enter a ${chalk.magenta('short description')} of the package:`)
             });
 
-            // Toggle options for files/directories
-            const { includeSrc, includeTest, includeDependabot, includeReadme, includeContributing, includeLicense } = await inquirer.prompt([
+            // toggle options for files/directories
+            const { includeSrc, includeTest, startGitRepo, includeDependabot, includeReadme, includeContributing, includeChangelog, includeLicense } = await inquirer.prompt([
                 {
                     type: 'confirm',
-                    name: 'includeSrc',
+                    name: 'includeSrc', // src prompt
                     message: chalk.cyan(`Would you like to include a ${chalk.magenta('src')} directory?`),
                     default: true
                 },
                 {
                     type: 'confirm',
-                    name: 'includeTest',
+                    name: 'includeTest', // test prompt
                     message: chalk.cyan(`Would you like to include a ${chalk.magenta('test')} directory?`),
                     default: true
                 },
                 {
                     type: 'confirm',
-                    name: 'includeDependabot',
+                    name: 'startGitRepo', // git init prompt
+                    message: chalk.cyan(`Would you like to initialise a local ${chalk.magenta('Git')} repository?`),
+                    default: true
+                },
+                {
+                    type: 'confirm',
+                    name: 'includeDependabot', // dependabot prompt
                     message: chalk.cyan(`Would you like to include ${chalk.magenta('Dependabot')}?`),
                     default: true
                 },
                 {
                     type: 'confirm',
-                    name: 'includeReadme',
+                    name: 'includeReadme', // readme prompt
                     message: chalk.cyan(`Would you like to include a ${chalk.magenta('README')} file?`),
                     default: true
                 },
                 {
                     type: 'confirm',
-                    name: 'includeContributing',
+                    name: 'includeContributing', // contributing prompt
                     message: chalk.cyan(`Would you like to include a ${chalk.magenta('contributing guidelines')} file?`),
                     default: true
                 },
                 {
                     type: 'confirm',
-                    name: 'includeLicense',
+                    name: 'includeLicense', // license prompt
                     message: chalk.cyan(`Would you like to include a ${chalk.magenta('LICENSE')} file?`),
                     default: true
                 }
             ]);
 
             // Create package structure and update package.json
-            await createPkgStructure(packageName, description, options, includeSrc, includeTest, includeDependabot, includeReadme, includeContributing, includeLicense);
+            await createPkgStructure(packageName, description, options, includeSrc, includeTest, startGitRepo, includeDependabot, includeReadme, includeContributing, includeLicense);
         } catch (err) {
             console.error(`Error initializing package: ${err}`);
         }
     });
 
-async function createPkgStructure(packageName, description, options, includeSrc, includeTest, includeDependabot, includeReadme, includeContributing, includeLicense) {
+async function createPkgStructure(packageName, description, options, includeSrc, includeTest, startGitRepo, includeDependabot, includeReadme, includeContributing, includeLicense) {
     const spinner = ora('Creating package structure...').start();
 
     try {
-        // Create src directory if selected
+        // src
         if (includeSrc) {
             const srcDir = path.join(process.cwd(), 'src');
             await fs.ensureDir(srcDir);
@@ -87,12 +93,22 @@ async function createPkgStructure(packageName, description, options, includeSrc,
             await fs.writeFile(indexFile, '', 'utf8');
         }
 
-        // Create test directory if selected
+        // test
         if (includeTest) {
             const testDir = path.join(process.cwd(), 'test');
             await fs.ensureDir(testDir);
         }
 
+        // start a git repo (git init)
+        if (startGitRepo) {
+            try {
+                await execa('git init');
+            } catch (err) {
+                console.error(chalk.red(`Error initializing Git repository: ${err}`));
+            }
+        }
+
+        // dependabot
         if (includeDependabot) {
             const WorkflowsFolder = path.join(process.cwd(), '.github');
             await fs.ensureDir(WorkflowsFolder);
@@ -102,20 +118,20 @@ async function createPkgStructure(packageName, description, options, includeSrc,
             await fs.writeFile(dependabotFile, 'version: 2\nupdates:\n  - package-ecosystem: "npm"\n    directory: "/"\n    schedule:\n     interval: "daily"', 'utf-8');
         }
 
-        // Create README.md with placeholder for description if selected
+        // template README.md
         if (includeReadme) {
             const readmeContent = `# ${packageName}\n\n${description}\n\n# Installation\n\n\`\`\`bash\nnpm install ${packageName}\n\`\`\`\n\n## Usage\n\n\`\`\`javascript\nconst ${packageName} = require('${packageName}')\n\n// (code goes here)\n\`\`\``;
             const readmeFile = path.join(process.cwd(), 'README.md');
             await fs.writeFile(readmeFile, readmeContent, 'utf8');
         }
 
-        // Create CONTRIBUTING.md if selected
+        // CONTRIBUTING.md
         if (includeContributing) {
             const contributingFile = path.join(process.cwd(), 'CONTRIBUTING.md');
             await fs.writeFile(contributingFile, '', 'utf8');
         }
 
-        // Create LICENSE file if selected
+        // LICENSE
         if (includeLicense) {
             const licenseFilePath = path.join(process.cwd(), 'LICENSE');
             await fs.writeFile(licenseFilePath, '', 'utf8');
